df278d65f0144fe0c743ec02e54631a86ede5734
Guilherme <gnailpo895@gmail.com>
2024-11-26 10:45:50 +0000

    Adicionei o botao do efeito Sepia

--- InterfaceGrafica.py
+++ InterfaceGrafica.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def desenhar_botao(imagem, texto, pos, cor_fundo=(50,50,50), cor_texto=(255,255,255)):
+def desenhar_botao(imagem, texto, pos, cor_fundo=(0,0,0), cor_texto=(255,255,255)):
    """
    :param imagem: Janela onde o botao sera desenhado
    :param texto: Texto exibido no botao
@@ -15,20 +15,22 @@ def desenhar_botao(imagem, texto, pos, cor_fundo=(50,50,50), cor_texto=(255,255,
    tamanho_texto = cv2.getTextSize(texto, fonte, 0.7, 2)[0]
    text_x = x + (w - tamanho_texto[0]) // 2
    text_y = y + (h - tamanho_texto[1]) // 2
-   cv2.putText(imagem, texto, (text_x, text_y), fonte, 0.7, cor_texto, 2)
+   cv2.putText(imagem, texto, (text_x, text_y), fonte, 0.7, cor_texto,)
 
 # Dimensões da janela e botões
-largura, altura = 800, 600
-botao_camara = (50, 500, 200, 50)
-botao_cinza = (300, 500, 200, 50)
-botao_fechar = (550, 500, 200, 50)
+largura, altura = 1000, 800 #tamanho da janela em si
+botao_camara = (0, 700, 200, 70)
+botao_EfeitoSepia = (250, 700, 200, 70)
+botao_cinza = (500, 700, 200, 70)
+botao_fechar = (750, 700, 200, 70)
 
 # Iniciar janela
 janela = np.zeros((altura, largura, 3), dtype=np.uint8)
 
 # Desenhar botões
-desenhar_botao(janela, "Iniciar Câmara", botao_camara)
-desenhar_botao(janela, "Converter para Cinzento", botao_cinza)
+desenhar_botao(janela, "Iniciar", botao_camara)
+desenhar_botao(janela, "Efeito Sepia", botao_EfeitoSepia)
+desenhar_botao(janela, "Converter para Cinza", botao_cinza)
 desenhar_botao(janela, "Fechar", botao_fechar)
 
 # Variáveis de estado
@@ -52,7 +54,7 @@ def eventos_rato(event, x, y, flags, param):
            if cap is None:
                cap = cv2.VideoCapture(0)
                if not cap.isOpened():
-                   print("Erro ao abrir a câmera.")
+                   print("Erro ao abrir a camera.")
                    cap = None
        elif clique_dentro_botao(x, y, botao_cinza):
            cinza_ativo = not cinza_ativo
@@ -81,8 +83,8 @@ while True:
 
    # Verificar tecla de saída
    key = cv2.waitKey(1) & 0xFF
-   if key == 27:  # ESC key
-       break
+   if key == 27:  # para sair do programa basta clicar na tecla ESC
+       break # que ira dar break (parar o programa)
 
 # Liberar recursos
 if cap is not None:
